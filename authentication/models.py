465d0965aafda572d0a7e4c2083e7a454bb32adf
Alexander Chai <alexanderchai416@gmail.com>
2021-11-10 01:39:13 +0800

    Made migration

--- authentication/models.py
+++ authentication/models.py
@@ -1,119 +1,128 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.fields import related
-from django.utils.translation import gettext_lazy as _
+# from django.db import models
+# from django.contrib.auth.models import User
+# from django.db.models.fields import related
+# from django.utils.translation import gettext_lazy as _
 
 
 
-# Create your models here.
-#Customer
-class Customer(models.Model):
-    user = models.OneToOneField(
-        User, 
-        on_delete=models.CASCADE,
-    )
-    phone = models.CharField()
+# # Create your models here.
+# #Customer
+# class Customer(models.Model):
+#     user = models.OneToOneField(
+#         User, 
+#         on_delete=models.CASCADE,
+#     )
+#     phone = models.CharField(
+#         max_length=20,
+#     )
     
-#Address
-class Address(models.Model):
-    created_by = models.ForeignKey(
-        Customer, 
-        on_delete=models.CASCADE,
-        related_name="created_by",
-    )
-    street = models.TextField()
-    city = models.TextField()
-    postal_code = models.TextField()
-    state = models.TextField()
-    country = models.TextField(
-        max_length=2,
-    )
+# #Address
+# class Address(models.Model):
+#     created_by = models.ForeignKey(
+#         Customer, 
+#         on_delete=models.CASCADE,
+#         related_name="created_by",
+#     )
+#     street = models.TextField()
+#     city = models.TextField()
+#     postal_code = models.TextField()
+#     state = models.TextField()
+#     country = models.TextField(
+#         max_length=2,
+#     )
 
-#Product
-class Product(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    image = models.ImageField()
-    price = models.DecimalField(
-        decimal_places=2,
-    )
+# #Product
+# class Product(models.Model):
+#     name = models.CharField(
+#         max_length=255,
+#     )
+#     description = models.TextField()
+#     image = models.ImageField()
+#     price = models.DecimalField(
+#         decimal_places=2,
+#         max_digits=6,
+#     )
 
-#Category
-class Category(models.Model):
-    name = models.CharField()
-    description = models.TextField()
+# #Category
+# class Category(models.Model):
+#     name = models.CharField(
+#         max_length=255,
+#     )
+#     description = models.TextField()
 
-#Plant Tip
-class PlantTip(models.Model):
-    sunlight = models.TextField()
-    water = models.TextField()
-    fertilizer_type = models.TextField()
-    temperature = models.TextField()
+# #Plant Tip
+# class PlantTip(models.Model):
+#     sunlight = models.TextField()
+#     water = models.TextField()
+#     fertilizer_type = models.TextField()
+#     temperature = models.TextField()
 
-#Cart
-class Cart(models.Model):
-    date_time_created = models.DateTimeField(
-        auto_now=True,
-    )
+# #Cart
+# class Cart(models.Model):
+#     date_time_created = models.DateTimeField(
+#         auto_now=True,
+#     )
 
-#Cart Item
-class CartItem(models.Model):
-    cart = models.ForeignKey(
-        Cart,
-        on_delete=models.CASCADE,
-    )
-    product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE,
-        related_name='cart_item_product_id',
-    )
-    quantity = models.IntegerField()
+# #Cart Item
+# class CartItem(models.Model):
+#     cart = models.ForeignKey(
+#         Cart,
+#         on_delete=models.CASCADE,
+#     )
+#     product = models.ForeignKey(
+#         Product,
+#         on_delete=models.CASCADE,
+#         related_name='cart_item_product_id',
+#     )
+#     quantity = models.IntegerField()
 
-#Order
-class Order(models.Model):
-    options = [
-        ('pending', 'Pending'),
-        ('received', 'Received'),
-        ('ready', 'Ready for Collect'),
-        ('sending', 'Sending'),
-        ('completed', 'Completed'),
-        ('cancelled', 'Cancelled'),
-    ]   
-    customer = models.ForeignKey(
-        Customer,
-        on_delete=models.CASCADE,
-    )
-    order_date = models.DateTimeField(
-        auto_now_add=True,
-    )
-    delivery_date = models.DateTimeField()
-    delivery_option = models.TextField()
-    order_status = models.CharField(
-        choices=options,
-        default='pending',
-    )
+# #Order
+# class Order(models.Model):
+#     options = [
+#         ('pending', 'Pending'),
+#         ('received', 'Received'),
+#         ('ready', 'Ready for Collect'),
+#         ('sending', 'Sending'),
+#         ('completed', 'Completed'),
+#         ('cancelled', 'Cancelled'),
+#     ]   
+#     customer = models.ForeignKey(
+#         Customer,
+#         on_delete=models.CASCADE,
+#     )
+#     order_date = models.DateTimeField(
+#         auto_now_add=True,
+#     )
+#     delivery_date = models.DateTimeField()
+#     delivery_option = models.TextField()
+#     order_status = models.CharField(
+#         max_length=255,
+#         choices=options,
+#         default='pending',
+#     )
 
-#Order Item
-class OrderItem(models.Model):
-    product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE,
-        related_name='order_item_product_id'
-    )
-    order = models.ForeignKey(
-        Order,
-        on_delete=models.CASCADE,
-    )
-    item_price = models.DecimalField(
-        decimal_places=2,
-    )
-    item_quantity = models.IntegerField()
+# #Order Item
+# class OrderItem(models.Model):
+#     product = models.ForeignKey(
+#         Product,
+#         on_delete=models.CASCADE,
+#         related_name='order_item_product_id'
+#     )
+#     order = models.ForeignKey(
+#         Order,
+#         on_delete=models.CASCADE,
+#     )
+#     item_price = models.DecimalField(
+#         decimal_places=2,
+#         max_digits=6,
+#     )
+#     item_quantity = models.IntegerField()
 
 
-#TODO: Create payment method
-#TODO: Fix Error
-#Payment
-# class Payment(models.Model):
-#     payment_method = models.TextField()
-#     payment_option = models.TextField()
+# #TODO: Create payment method
+# #TODO: Fix Error
+# #Payment
+# # class Payment(models.Model):
+# #     payment_method = models.TextField()
+# #     payment_option = models.TextField()
 
