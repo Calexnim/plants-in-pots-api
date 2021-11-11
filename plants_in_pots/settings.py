e33d09d85ecb192debad3bda8e1df951e438494f
Alexander Chai <alexanderchai416@gmail.com>
2021-11-12 02:34:19 +0800

    Added rest_framework_token

--- plants_in_pots/settings.py
+++ plants_in_pots/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
@@ -68,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTH_USER_MODEL = 'api.User'
 
 WSGI_APPLICATION = 'plants_in_pots.wsgi.application'
 
@@ -129,3 +132,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
