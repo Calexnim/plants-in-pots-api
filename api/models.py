f369116b7e2ed0649b5d67912c5526cd8be2bf9f
Alexander Chai <alexanderchai416@gmail.com>
2021-11-12 02:32:51 +0800

    Change Customer model to User

--- api/models.py
+++ api/models.py
@@ -1,25 +1,101 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db.models.fields import related
 from django.utils.translation import gettext_lazy as _
 
 
 
 # Create your models here.
-#Customer
-class Customer(models.Model):
-    user = models.OneToOneField(
-        User, 
-        on_delete=models.CASCADE,
+#UserManager 
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have an username")
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            username=username
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+#User
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email', 
+        max_length=60, unique=True
+    )
+    username = models.CharField(
+        max_length=30, 
+        unique=True
+    )
+    date_joined = models.DateTimeField(
+        verbose_name='date joined', 
+        auto_now_add=True
+    )
+    last_login = models.DateTimeField(
+        verbose_name='last login',
+        auto_now_add=True
+    )
+    is_admin = models.BooleanField(
+        default=False
+    )
+    is_active = models.BooleanField(
+        default=True
+    )
+    is_staff = models.BooleanField(
+        default=False
+    )
+    is_superuser = models.BooleanField(
+        default=False
     )
     phone = models.CharField(
         max_length=20,
+        null=True,
+        blank=True,
+    )
+    first_name = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+    )
+    last_name = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
     )
     
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    object = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, permission, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
+
 #Address
 class Address(models.Model):
     created_by = models.ForeignKey(
-        Customer, 
+        User, 
         on_delete=models.CASCADE,
         related_name="created_by",
     )
@@ -86,8 +162,8 @@ class Order(models.Model):
         ('completed', 'Completed'),
         ('cancelled', 'Cancelled'),
     ]   
-    customer = models.ForeignKey(
-        Customer,
+    user = models.ForeignKey(
+        User,
         on_delete=models.CASCADE,
     )
     order_date = models.DateTimeField(
@@ -120,7 +196,6 @@ class OrderItem(models.Model):
 
 
 #TODO: Create payment method
-#TODO: Fix Error
 #Payment
 # class Payment(models.Model):
 #     payment_method = models.TextField()
