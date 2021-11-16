4fcebab992910ca7f35548774c7bda663bc42dab
Alexander Chai <alexanderchai416@gmail.com>
2021-11-16 16:45:39 +0800

    Created login url

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,11 @@ from api.views import(
     registration_view,
     user_retrieve,
 )
-
+from rest_framework.authtoken.views import obtain_auth_token
 app_name = "api"
 
 urlpatterns = [
     path('register', registration_view, name='register'),
-    path('<int:pk>/', user_retrieve)
+    path('<int:pk>/', user_retrieve),
+    path('login', obtain_auth_token, name="login"),
 ]
