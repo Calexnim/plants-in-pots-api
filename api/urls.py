46be556c6c681bc86d447ef99a49c435e1fdfcc1
Alexander Chai <alexanderchai416@gmail.com>
2021-12-07 21:25:01 +0800

    Update on URL

--- api/urls.py
+++ api/urls.py
@@ -10,11 +10,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 app_name = "api"
 
 router = DefaultRouter()
-router.register('', CategoryViewSet, basename='category')
+router.register(r'category', CategoryViewSet, basename='category')
 # urlpatterns = router.urls
 urlpatterns = [
     path('account/register', registration_view, name='register'),
     path('account/<int:pk>/', user_retrieve),
     path('account/login', obtain_auth_token, name="login"),
-    path(r'category', include(router.urls))
+    path('', include(router.urls))
 ]
