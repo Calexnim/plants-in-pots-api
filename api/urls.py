a0af246bc0afd936946caaad8e01766d8fe61798
Alexander Chai <alexanderchai416@gmail.com>
2021-12-07 17:31:31 +0800

    Added category route

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
+from django.conf.urls import url, include
 from api.views import(
     registration_view,
     user_retrieve,
+    CategoryViewSet
 )
+from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 app_name = "api"
 
+router = DefaultRouter()
+router.register('', CategoryViewSet, basename='category')
+# urlpatterns = router.urls
 urlpatterns = [
-    path('register', registration_view, name='register'),
-    path('<int:pk>/', user_retrieve),
-    path('login', obtain_auth_token, name="login"),
+    path('account/register', registration_view, name='register'),
+    path('account/<int:pk>/', user_retrieve),
+    path('account/login', obtain_auth_token, name="login"),
+    path(r'category', include(router.urls))
 ]
