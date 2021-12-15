b5b482b065200b4e5ef37bf5a86884bdc22d1e65
Alexander Chai <alexanderchai416@gmail.com>
2021-12-15 22:27:31 +0800

    Register product in admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from api.models import Category, User
+from api.models import Category, Product, User
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Category)
+admin.site.register(Product)
