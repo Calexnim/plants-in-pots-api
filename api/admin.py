b6de69b59b493d79010dea99a3ab82fb6090d488
Alexander Chai <alexanderchai416@gmail.com>
2021-12-30 18:08:02 +0800

    Register Pot

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from api.models import Category, Product, User
+from api.models import Category, Product, User, Pot
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Pot)
