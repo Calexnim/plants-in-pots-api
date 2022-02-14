e28c1773948b8fc08013fc3ffc4e5692da9f2932
Alexander Chai <alexanderchai416@gmail.com>
2022-02-14 13:06:43 +0800

    Cart

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
+from posixpath import basename
 from django.urls import path
 from django.conf.urls import include
+# from api.models import CartItem
 from api.views import(
+    CartItemViewSet,
+    CartViewSet,
+    CustomAuthToken,
     PotViewSet,
     ProductViewSet,
     registration_view,
@@ -15,10 +20,12 @@ router = DefaultRouter()
 router.register(r'category', CategoryViewSet, basename='category')
 router.register(r'product', ProductViewSet, basename='product')
 router.register(r'pot', PotViewSet, basename='pot')
+router.register(r'cart', CartViewSet, basename='cart')
+router.register(r'cart-item', CartItemViewSet, basename='cart-item')
 # urlpatterns = router.urls
 urlpatterns = [
     path('account/register', registration_view, name='register'),
     path('account/<int:pk>/', user_retrieve),
-    path('account/login', obtain_auth_token, name="login"),
+    path('account/login', CustomAuthToken.as_view(), name="login"),
     path('', include(router.urls))
 ]
