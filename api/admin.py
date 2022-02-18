73e17dc2490caffeb7f0efbaf8c5b055cf2e9527
Alexander Chai <alexanderchai416@gmail.com>
2022-02-18 18:25:48 +0800

    Added filter for product and order

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from api.models import Category, Product, User, Pot
+from api.models import CartItem, Category, Fertilizer, Order, OrderItem, Product, User, Pot, Cart
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    list_filter = ['category']
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+    list_display = ['id', 'order_status']
+    list_filter = ['order_status']
 
 admin.site.register(User)
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Pot)
+admin.site.register(Cart)
+# admin.site.register(CartItem)
+admin.site.register(Fertilizer)
+admin.site.register(Order, OrderAdmin)
